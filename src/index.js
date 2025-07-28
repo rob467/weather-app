72f2931f92ee78ae45d5896fbc47382a8b79651e
rob467 <133133177+rob467@users.noreply.github.com>
2025-07-28 21:11:13 +0200

    Remove unnecessary template files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import populateLocalStorage from './LoadLocalStorage.js';
-import { renderSidebar } from './RenderSidebarComponent.js';
 import './styles.css';
 import './reset-styles.css';
-
-populateLocalStorage();
-renderSidebar();

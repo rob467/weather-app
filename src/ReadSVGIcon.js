5206f0320b3554112d24fdc0f82d8772ac1510c6
rob467 <133133177+rob467@users.noreply.github.com>
2025-08-08 20:33:37 +0200

    Renders weather icons

---
+++ src/ReadSVGIcon.js
@@ -0,0 +1,50 @@
+import { createHtmlEl } from './AddDOMComponents.js';
+
+function getSVGMap() {
+  const SVGMap = {
+    'clear-day': 'clear-day.svg',
+    'clear-night': 'clear-night.svg',
+    cloudy: 'cloudy.svg',
+    fog: 'fog.svg',
+    hail: 'hail.svg',
+    'partly-cloudy-day': 'partly-cloudy-day.svg',
+    'partly-cloudy-night': 'partly-cloudy-night.svg',
+    'rain-snow-showers-day': 'rain-snow-showers-day.svg',
+    'rain-snow-showers-night': 'rain-snow-showers-night.svg',
+    'rain-snow': 'rain-snow.svg',
+    rain: 'rain.svg',
+    'showers-day': 'showers-day.svg',
+    'showers-night': 'showers-night.svg',
+    sleet: 'sleet.svg',
+    'snow-showers-day': 'snow-showers-day.svg',
+    'snow-showers-night': 'snow-showers-night.svg',
+    snow: 'snow.svg',
+    'thunder-rain': 'thunder-rain.svg',
+    'thunder-showers-day': 'thunder-showers-day.svg',
+    'thunder-showers-night': 'thunder-showers-night.svg',
+    thunder: 'thunder.svg',
+    wind: 'wind.svg',
+  };
+
+  return SVGMap;
+}
+
+function renderSVGIcon(iconValue, parentContainer) {
+  const svgFilename = getSVGMap()[iconValue];
+  if (!svgFilename) {
+    console.error('No SVG found for value', iconValue);
+    return;
+  }
+  import(`./svgs/visualcrossing-icons/${svgFilename}`).then((svgModule) => {
+    createHtmlEl({
+      tag: 'img',
+      parent: parentContainer,
+      props: {
+        src: svgModule.default,
+        className: 'svg-icon',
+      },
+    });
+  });
+}
+
+export default renderSVGIcon;
--- src/RenderWeatherInfo.js
+++ src/RenderWeatherInfo.js
@@ -1,9 +1,11 @@
-import { createHtmlEl } from './AddDOMComponents.js';
+import { createHtmlEl, removeAllChildren } from './AddDOMComponents.js';
 import formatDates from './DateUtils.js';
+import renderSVGIcon from './ReadSVGIcon.js';
 
 // Function for rendering the parsed weather data
 function renderWeatherInfo(parsedWeatherDataArray) {
   const weatherContainer = document.querySelector('.weather-container');
+  removeAllChildren(weatherContainer);
   // 1. Create individual containers for each day
   parsedWeatherDataArray.forEach((day) => {
     const dayContainer = createHtmlEl({
@@ -23,17 +25,25 @@ function renderWeatherInfo(parsedWeatherDataArray) {
       ],
     });
 
+    const iconContainer = createHtmlEl({
+      parent: dayContainer,
+      props: { className: 'icon-container' },
+    });
+
+    renderSVGIcon(day['icon'], iconContainer);
+
     const precipContainer = createHtmlEl({
       parent: dayContainer,
-      children: [
-        createHtmlEl({
-          tag: 'span',
-          textContent: `${day['precipprob'] ? `${Math.round(day['precipprob'] / 5) * 5} %` : ''}`,
-        }),
-      ],
     });
-    // 2. Show weather data by day: temp-max/temp-min, percentage precipition and weather icon
-    // 3. Hourly container: Could initially be hidden, but shows the hour by hour weather for a clicked day
+
+    createHtmlEl({
+      tag: 'span',
+      parent: precipContainer,
+      textContent:
+        Math.round(day['precipprob'] / 5) * 5
+          ? `${Math.round(day['precipprob'] / 5) * 5} %`
+          : '',
+    });
   });
 }
 
